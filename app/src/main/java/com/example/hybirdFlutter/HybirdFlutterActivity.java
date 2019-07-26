35055a76e9714890b934734bef2aea23e5201e71
natureday <1004934782@qq.com>
2019-07-26 15:10:21 +0800

    update project

--- app/src/main/java/com/example/hybirdFlutter/HybirdFlutterActivity.java
+++ app/src/main/java/com/example/hybirdFlutter/HybirdFlutterActivity.java
@@ -1,12 +1,15 @@
 package com.example.hybirdFlutter;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.view.Window;
 
 import io.flutter.facade.Flutter;
+import io.flutter.plugin.common.BasicMessageChannel;
+import io.flutter.plugin.common.StringCodec;
 import io.flutter.view.FlutterView;
 
 public class HybirdFlutterActivity extends AppCompatActivity {
@@ -31,7 +34,21 @@ public class HybirdFlutterActivity extends AppCompatActivity {
         addContentView(mFlutterView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
     }
 
-    private void initBridge(){
+    private void initBridge() {
+        BasicMessageChannel basicMessageChannel = new BasicMessageChannel(mFlutterView, "BasicMessageChannel", StringCodec.INSTANCE);
+        basicMessageChannel.setMessageHandler(new BasicMessageChannel.MessageHandler<String>() {
+            @Override
+            public void onMessage(@Nullable String s, @NonNull BasicMessageChannel.Reply<String> reply) {
+                //第一个参数就是dart端发送过来的消息
+                reply.reply("basicMessageChannel收到消息");//可以通过reply回复消息
+            }
 
+        });
+        basicMessageChannel.send("发送给dart端的消息", new BasicMessageChannel.Reply<String>() {
+            @Override
+            public void reply(@Nullable String s) {//来自dart的反馈
+
+            }
+        });
     }
 }
