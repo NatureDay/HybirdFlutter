dea006b5013d1b49410e9d832343b15eadd2e5b7
natureday <1004934782@qq.com>
2019-07-26 15:04:22 +0800

    update project

---
+++ my_flutter/lib/bridge.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/services.dart'; //需要导入包
+
+/// 与native进行通信
+class Bridge {
+  static const BasicMessageChannel _basicMessageChannel =
+      BasicMessageChannel('BasicMessageChannel', StringCodec());
+
+//使用BasicMessageChannel接收来自native的消息，并向native回复
+  void _reviceData() {
+    _basicMessageChannel.setMessageHandler(_handlerMessage);
+  }
+
+  Future<String> _handlerMessage(message) async {
+    return '收到native的消息${message}';
+  }
+
+//使用BasicMessageChannel向native发送消息，并接收native的回复
+  void _sendMessage() async {
+    //response为native回复的消息
+    try {
+      String response = await _basicMessageChannel.send('发送给native的消息');
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+}
